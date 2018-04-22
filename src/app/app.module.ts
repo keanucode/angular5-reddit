6e265ac1dc47bb70d50f61c8996e60fbc907d6d1
Keanu Huang <keanu.code@gmail.com>
2018-04-22 17:14:14 +0800

    Import ng-zorro-antd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,11 +9,15 @@ import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
 
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgZorroAntdModule.forRoot(),
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
